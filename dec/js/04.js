3c55298414259b0419e308b4dd97153517fbeea0
Asier Antillaque <aantillaquel25@fpsanturtzilh.eus>
2026-09-18 08:40:34 +0200

    First commit

---
+++ dec/js/04.js
@@ -0,0 +1,17 @@
+let egia  = true;
+console.log(egia);
+
+let gezurra = false;
+console.log(gezurra);
+
+let nuloa = null;
+console.log(nuloa);
+
+let adina = 18;
+console.log(adina);
+
+let tenperatura = 22.5;
+console.log(tenperatura);
+
+let saldo = -150.75;
+console.log(saldo);
