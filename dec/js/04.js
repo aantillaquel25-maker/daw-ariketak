1ccfcfbbc11029861d9ed063706354cbfcc4c8b5
Asier Antillaque <aantillaquel25@fpsanturtzilh.eus>
2026-09-24 08:29:17 +0200

    ariketas js,html y cv.

--- dec/js/04.js
+++ dec/js/04.js
@@ -1,17 +1,46 @@
-let egia  = true;
-console.log(egia);
+console.log(verdadero);
 
-let gezurra = false;
-console.log(gezurra);
+let falso = false;
+console.log(falso);
 
-let nuloa = null;
-console.log(nuloa);
+let nulo = null;
+console.log(nulo);
 
-let adina = 18;
-console.log(adina);
+let edad = 18;
+console.log(edad);
 
-let tenperatura = 22.5;
-console.log(tenperatura);
+let temperatura = 22.5;
+console.log(temperatura);
 
 let saldo = -150.75;
 console.log(saldo);
+
+
+// Infinity
+console.log(42 / 0);
+// -Infinity
+console.log(-42 / 0);
+
+let nombre = "Rubén";
+let ciudad = 'Bilbao';
+let saludo = `Hola`;
+console.log("Hola "+nombre)
+console.log("Hola %s",nombre)
+console.log(`Hola ${nombre}`)
+
+let variable;
+// undefined
+console.log(variable);
+
+const sym1 = Symbol();
+console.log(sym1);
+console.log(sym1.description);
+
+const sym2 = Symbol(42);
+console.log(sym2);
+console.log(sym2.description);
+
+
+const sym3 = Symbol("hola");
+console.log(sym3);
+console.log(sym3.description);
